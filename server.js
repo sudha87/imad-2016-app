15eb8ac110f2eea773f03d1ccf5c3199b94f6423
sudha87 <sudha.sow@gmail.com>
2016-10-10 16:03:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.get('/article-one',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function (req, res) {
-    res.send("Article two is served");
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function (req, res) {
     res.send("Article three is served");
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  resres.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
