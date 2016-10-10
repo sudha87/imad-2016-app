6b81b5c57b3f085f12575ffb20845a5d9d85839e
sudha87 <sudha.sow@gmail.com>
2016-10-10 20:17:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,14 +6,31 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne= 
+var articles = 
 {
-    title:'Article one',
-    heading:'Article one is running',
-    date: 'oct 10 2016',
-    content:
-    '<p>Modern application development course is teached by hasura team.It is very instresting to develop web application.</p>'
+article-one 
+{
+title:'Article one';
+heading:'Article one is running';
+date: 'oct 10 2016';
+content:
+'<p>Modern application development course is teached by hasura team.It is very insteresting to develop web application.</p>'
 };
+article-two =
+{
+title:'Article two',
+heading:'Article two is running',
+date: 'oct 10 2016',
+content:
+'<p>Modern application development course is teached by hasura team.It is very instresting to develop web application.article two</p>'
+};
+article-three={
+title:'Article three',
+heading:'Article three is running',
+date: 'oct 10 2016',
+content:
+'<p>Modern application development course is teached by hasura team.It is very instresting to develop web application.article three</p>'};
+}
 function createTemplate (data)
 {
 var title=data.title;
@@ -50,9 +67,12 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function (req, res) {
+app.get('/:articleName',function (req, res) {
     //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-    res.send(createTemplate(articleOne));
+    //articleName=article-one
+    //articles[articleName]
+    var articleName=res.params.articleName
+    res.send(createTemplate(articles[articleName]));
 });
 app.get('/article-two',function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
