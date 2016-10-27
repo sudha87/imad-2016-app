2f0ee129dcf4a460d7b384121d814aa9e4d3abec
sudha87 <sudha.sow@gmail.com>
2016-10-27 16:17:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,9 +75,9 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName',function (req, res) {
     //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-    //articleName=article-one
-    //articles[articleName] {}
-    var articleName=res.param.articleName;
+    //articleName==article-one
+    //articles[articleName]=={} content object for article one
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
