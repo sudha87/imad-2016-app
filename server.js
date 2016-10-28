518eb989833d83398328e342f0efb1e2be163733
sudha87 <sudha.sow@gmail.com>
2016-10-28 12:01:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,40 +34,43 @@ var articles =
     '<p>Modern application development course is teached by hasura team.It is very instresting to develop web application.article three</p>'
   }
 };
-            function createTemplate (data)
-            {
-                var title = data.title;
-                    var heading = data.heading;
-                    var date = data.date;
-                    var content = data.content;
-                    var htmlTemplate = 
-                    '<html>
-                    <head>
-                      <title>
-                        ${title}
-                      </title>
-                        <link href="/ui/style.css" rel="stylesheet" />
-                     </head>
-                       <body>
-                        <div class="container">    
-                         <div>
-                          <a href="/">Home</a>
-                         </div>
-                        <hr/>
-                        <h3>
-                        ${heading}
-                        </h3>
-                        <div>
-                        ${date}
-                        </div>
-                        <div>
-                        ${content}
-                        </div>
-                        </div>
-                    </body>
-                    </html>';
-                    return htmlTemplate; 
-                } 
+                    function createTemplate (data)
+                    {
+                            var title = data.title;
+                            var heading = data.heading;
+                            var date = data.date;
+                            var content = data.content;
+                            var htmlTemplate = 
+                            <html>
+                            <head>
+                            <title>
+                            ${title}
+                            </title>
+                                <meta name="viewport" contect="width=device-width, initial-scale=1/>
+                                 <link href="/ui/style.css" rel="stylesheet" />
+                                 </head>
+                                   <body>
+                                    <div class="container">    
+                                     <div>
+                                      <a href="/">Home</a>
+                                     </div>
+                                    <hr/>
+                                    <h3>
+                                    ${heading}
+                                    </h3>
+                                    <div>
+                                    ${date}
+                                    </div>
+                                    <div>
+                                    ${content}
+                                    </div>
+                                    </div>
+                                </body>
+                                </html>
+                                ';
+                                return htmlTemplate; 
+                                  
+                            } 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
