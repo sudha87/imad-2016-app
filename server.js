c9149c6ef479485cf425fa7fc44dee10c44533ad
sudha87 <sudha.sow@gmail.com>
2016-10-28 12:35:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,39 +36,40 @@ var articles =
 };
 function createTemplate (data) {
     var title = data.title;
-    var heading = data.heading;
     var date = data.date;
+    var heading = data.heading;
     var content = data.content;
-    var htmlTemplate = '
+    
+    var htmlTemplate = `
     <html>
-    <head>
-    <title>
-    ${title}
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1/>
-    <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-    <div class="container">    
-    <div>
-    <a href="/">Home</a>
-    </div>
-    <hr/>
-    <h3>
-    ${heading}
-    </h3>
-    <div>
-    ${date}
-    </div>
-    <div>
-    ${content}
-    </div>
-    </div>
-    </body>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
+                ${content}
+              </div>
+          </div>
+      </body>
     </html>
-    ';
-    return htmlTemplate; 
-} 
+    `;
+    return htmlTemplate;
+}
                     
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
