7360567ffaf31ee330512157399b97eb1fd9504b
sudha87 <sudha.sow@gmail.com>
2016-10-31 14:19:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ function moveRight()
     img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick=function() {
-    var interval=setIntervel(moveRight,100);
+    var interval=setInterval(moveRight,100);
     //img.style.marginLeft= '100px';
 };
