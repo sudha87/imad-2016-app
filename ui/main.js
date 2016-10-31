021fb02cf3e6ecc259f69ad4e50cfac64d50e091
sudha87 <sudha.sow@gmail.com>
2016-10-31 13:39:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+//Move the image
+var img=document.getElementById('madi');
+img.onclick=function() {
+    img.style.marginleft= '100px';
+};
