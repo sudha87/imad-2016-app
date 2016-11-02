aadd0936264b19e7837869ac34e86aeaa743b8d3
sudha87 <sudha.sow@gmail.com>
2016-11-02 14:39:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,64 +1,78 @@
-var button=document.getElementById('counter');
-
-button.onclick=function ()
-{
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function (){
-        if (request.readyState===XMLHttpRequest.DONE)
-        {
-            if (request.status===200)
+    var button=document.getElementById('counter');
+    
+    button.onclick=function ()
+    {
+        var request=new XMLHttpRequest();
+        request.onreadystatechange=function (){
+            if (request.readyState===XMLHttpRequest.DONE)
             {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                if (request.status===200)
+                {
+                    var counter=request.responseText;
+                    var span=document.getElementById('count');
+                    span.innerHTML=counter.toString();
+                }
             }
-        }
+        };
+        
+       request.open('GET','http://sudha87.imad.hasura-app.io/counter',true );
+       request.send(null);
+        
+    };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    var submit=document.getElementById('submit_btn');
+    submit.onclick=function()
+    {
+        var request=new XMLHttpRequest();
+        request.onreadystatechange=function (){
+            if (request.readyState===XMLHttpRequest.DONE)
+            {
+                if (request.status===200)
+                {
+                   // var names=['name1','name2','name3','name4'];
+                    var names=request.responseText;
+                    names=JSON.parse(names);
+                    var list='';
+                     for (var i=0;i<names.length;i++)
+                      {
+                         list +='<li>' +names[i]+ '<li>';
+                      }
+                  var ul = document.getElementById('namelist');
+                   ul.innerHTML=list;
+                }
+            }
+        };
+        request.open('GET','http://sudha87.imad.hasura-app.io/submit-name?name=' +name ,true );
+        request.send(null);
+        
     };
     
-   request.open('GET','http://sudha87.imad.hasura-app.io/counter',true );
-   request.send(null);
     
-};
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-var submit=document.getElementById('submit_btn');
-submit.onclick=function()
-{
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0;i<names.length;i++)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*console.log('Loaded!');
+    var element=document.getElementById('main-text');
+    element.innerHTML='New value';
+    //Move the image
+    var img=document.getElementById('madi');
+    var marginLeft=0;
+    function moveRight()
     {
-        list +='<li>' +names[i]+ '<li>';
+        marginLeft=marginLeft + 1;
+        img.style.marginLeft=marginLeft + 'px';
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML=list;
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='New value';
-//Move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick=function() {
-    var interval=setInterval(moveRight,50);
-    //img.style.marginLeft= '100px';
-};*/
+    img.onclick=function() {
+        var interval=setInterval(moveRight,50);
+        //img.style.marginLeft= '100px';
+    };*/
