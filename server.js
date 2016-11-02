0fe004796153814e3fa469016394f1451b267e88
sudha87 <sudha.sow@gmail.com>
2016-11-02 13:52:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,9 +97,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names =[];
-app.get('/submit-name/:name',function (req,res)
+app.get('/submit-name',function (req,res)
 {
-    var name=req.params.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
