b809820bcec58537d27835ce2fe607ce7c8a5f5a
sudha87 <sudha.sow@gmail.com>
2016-11-03 14:36:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,10 +108,21 @@ app.get('/submit-name',function (req,res)
     names.push(name);
     res.send(JSON.stringify(names));
 });
-app.get('/:articleName',function (req, res) {
-    
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('article/:articleName',function (req, res) {
+    pool.query("SELECT * FROM title=",+req.params.articleName,function (err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }  
+      else{
+          if(result.rows.length===0){
+              res.status(404).send("Article Not Found");
+              }
+          else{
+              var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+    });
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
