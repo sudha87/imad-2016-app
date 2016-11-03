e0545fe2dd66f8afb2977a8578b4804d201b742a
sudha87 <sudha.sow@gmail.com>
2016-11-03 14:45:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,8 @@ app.get('/submit-name',function (req,res)
     res.send(JSON.stringify(names));
 });
 app.get('/articles/:articleName',function (req, res) {
-    pool.query("SELECT * FROM title=" +req.params.articleName,function (err,result){
+    pool.query("SELECT * FROM title= '" + req.params.articleName +" ' ",function (err,result)
+    {
       if(err){
           res.status(500).send(err.toString());
       }  
