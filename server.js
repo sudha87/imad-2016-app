83a2b6366c721959de63f87402efc51637cc53fd
sudha87 <sudha.sow@gmail.com>
2016-11-03 14:11:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config= {
+    user: 'sudha87',
+    database:'sudha87',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -74,6 +81,19 @@ function createTemplate (data) {
                     
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool= new Pool(config);
+app.get('/test-db',function (req,res)
+{
+    pool.query('SELECT * FROM test',function (err,result)
+    {
+       if(err) {
+           res.status(500).send(err.toString());
+       }  
+       else {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 var counter=0;
 app.get('/counter', function (req,res)
