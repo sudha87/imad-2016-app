2cdf1198806757a5ecfe36f86213b861421bee3c
sudha87 <sudha.sow@gmail.com>
2016-11-04 17:18:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/submit-name',function (req,res)
 });
 app.get('/articles/:articleName',function (req, res) {
     // SELECT * FROM article WHERE title='\'; DELETE WHERE a = '\asdf'
-    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName],function (err,result)
+    pool.query("SELECT * FROM article WHERE title = $1" , [req.params.articleName], function (err,result)
     {
       if(err){
           res.status(500).send(err.toString());
