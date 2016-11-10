583d016d5cc72ee566bdc559d47cadc91e7fa4c9
sudha87 <sudha.sow@gmail.com>
2016-11-10 16:55:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ function hash (input,salt) {
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
-app.get('/hash/:input',function (req,res){
+app.get('/hash/:input',function (req,res) {
     var hashedString=hash(req.params.input,'this is some string');
     res.send(hashedString);
 });
