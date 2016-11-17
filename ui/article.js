c703a1bd6069ac133f9f3b4ac42ba477218b9c2d
sudha87 <sudha.sow@gmail.com>
2016-11-17 22:56:44 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -97,3 +97,26 @@ function loadComments () {
 // The first thing to do is to check if the user is logged in!
 loadLogin();
 loadComments();
+
+
+var button=document.getElementById('counter');
+    
+    button.onclick=function ()
+    {
+        var request=new XMLHttpRequest();
+        request.onreadystatechange=function (){
+            if (request.readyState===XMLHttpRequest.DONE)
+            {
+                if (request.status===200)
+                {
+                    var counter=request.responseText;
+                    var span=document.getElementById('count');
+                    span.innerHTML=counter.toString();
+                }
+            }
+        };
+        
+       request.open('GET','http://sudha87.imad.hasura-app.io/counter',true );
+       request.send(null);
+        
+    };
