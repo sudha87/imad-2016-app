f6ab9924fb607fd4cefe7c9225d878769538c545
sudha87 <sudha.sow@gmail.com>
2016-11-18 08:04:06 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -90,27 +90,7 @@ function loadComments () {
                 comments.innerHTML('Oops! Could not load comments!');
             }
         }
-        var button=document.getElementById('counter');
-    
-    button.onclick=function ()
-    {
-        var request=new XMLHttpRequest();
-        request.onreadystatechange=function (){
-            if (request.readyState===XMLHttpRequest.DONE)
-            {
-                if (request.status===200)
-                {
-                    var counter=request.responseText;
-                    var span=document.getElementById('count');
-                    span.innerHTML=counter.toString();
-                }
-            }
-        };
         
-       request.open('GET','http://sudha87.imad.hasura-app.io/articles/counter',true );
-       request.send(null);
-        
-    };
     };
     
     request.open('GET', '/get-comments/' + currentArticleTitle, true);
