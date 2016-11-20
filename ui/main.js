3ea006213820124699b6ae226375676f1c4db5f4
sudha87 <sudha.sow@gmail.com>
2016-11-20 19:43:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -153,7 +153,28 @@ loadArticles();
         var interval=setInterval(moveRight,50);
     };
 
-
+var button=document.getElementById('counter');
+    
+    button.onclick=function ()
+    {
+        var request=new XMLHttpRequest();
+        request.onreadystatechange=function (){
+            if (request.readyState===XMLHttpRequest.DONE)
+            {
+                if (request.status===200)
+                {
+                    var counter=request.responseText;
+                    var span=document.getElementById('count');
+                    span.innerHTML=counter.toString();
+                }
+            }
+        };
+        
+       request.open('GET','http://sudha87.imad.hasura-app.io/counter',true );
+       request.send(null);
+        
+    };
+    
 
 
 
