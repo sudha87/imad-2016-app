d6cc079d297345eac0a3cbd9fbba6f456db10e79
sudha87 <sudha.sow@gmail.com>
2016-11-22 15:00:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,35 +20,6 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-/*var articles =
-{
-
-  'article-one':
- {
-   title:'Article one',
-   heading:'Article one is running',
-   date: 'oct 10 2016',
-   content:
-    '<p>Modern application development course is teaching by hasura team.It is very interesting to develop web application.</p>'
- },
- 'article-two':
- {
-   title:'Article two',
-   heading:'Article two is running',
-   date: 'oct 10 2016',
-   content:
-    '<p>Modern application development course is teaching by hasura team.It is very interesting to develop web application.article two</p>'
- }, 
-  'article-three':
- {
-   title:'Article three',
-   heading:'Article three is running',
-   date: 'oct 10 2016',
-   content:
-    '<p>Modern application development course is teached by hasura team.It is very instresting to develop web application.article three</p>'
-  }
-};*/
-
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
