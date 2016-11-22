3f88e953ac9ca6d447f1297845c9a58fa9541663
sudha87 <sudha.sow@gmail.com>
2016-11-22 16:33:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -155,10 +155,10 @@ loadArticles();
         var interval=setInterval(moveRight,50);
     };
     
-
-var button=document.getElementById('counter');
-   if(counter) 
+ if(counter) 
    {
+var button=document.getElementById('counter');
+  
     button.onclick=function ()
     {
         var request=new XMLHttpRequest();
