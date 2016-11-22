6b254d564b2f285cdab5449f527b3dc49d4e7efc
sudha87 <sudha.sow@gmail.com>
2016-11-22 16:40:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -170,8 +170,8 @@ var button=document.getElementById('counter');
                 {
                     var counter=request.responseText;
                     var span=document.getElementById('count');
-                    //span.innerHTML=counter.toString();
-                    counter.innerHTML = count.toString();
+                    span.innerHTML=counter.toString();
+                    //counter.innerHTML = count.toString();
                 }
             }
     }
