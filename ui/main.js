135f2ca72acf5bff511e49cf7b775a6f19deefed
sudha87 <sudha.sow@gmail.com>
2016-11-26 19:05:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,8 +8,8 @@ function loadLoginForm () {
         <input type="text" class="form-control" id="username" placeholder="username" /><br><br>
         <input type="password" class="form-control" id="password" placeholder="password"/>
         <br/><br/>
-        <input type="submit" id="login_btn" class="form-control" value="Login" />
-        <input type="submit" id="register_btn"  class="form-control" value="Register" />
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
         </div>
         </form>
         `;
