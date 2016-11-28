36268e30920ec5031e949ac6f3b9c2acfb89db68
sudha87 <sudha.sow@gmail.com>
2016-11-28 17:36:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,9 +69,7 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
-});
+
 function hash (input,salt) {
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
