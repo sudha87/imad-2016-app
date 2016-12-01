3dca3b8df63e7f89417b56ea72605de38b18c9e8
sudha87 <sudha.sow@gmail.com>
2016-12-01 12:09:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -163,34 +163,34 @@ loadArticles();
     };
     
  
-var button=document.getElementById('counter');
-  
-    button.onclick=function ()
-    {
-        if(counter) 
-       {
-        var request=new XMLHttpRequest();
-        request.onreadystatechange=function ()
+    var button=document.getElementById('counter');
+      
+        button.onclick=function ()
         {
-            if (request.readyState===XMLHttpRequest.DONE)
+            if(counter) 
+           {
+            var request=new XMLHttpRequest();
+            request.onreadystatechange=function ()
             {
-                if (request.status===200)
+                if (request.readyState===XMLHttpRequest.DONE)
                 {
-                    var counter=request.responseText;
-                    var span=document.getElementById('count');
-                    //span.innerHTML=counter.toString();
-                    count.innerHTML=counter.toString();
-                    //counter.innerHTML = count.toString();
+                    if (request.status===200)
+                    {
+                        var counter=request.responseText;
+                        var span=document.getElementById('count');
+                        //span.innerHTML=counter.toString();
+                        count.innerHTML=counter.toString();
+                        //counter.innerHTML = count.toString();
+                    }
                 }
             }
-      }
-    };
-        
-       //request.open('GET',window.location.protocol+''+window.location.host+'/counter',true );
-       request.open('GET','http://sudha87.imad.hasura-app.io/counter',true );
-       request.send(null);
-        
-    }
+           };
+            
+           //request.open('GET',window.location.protocol+''+window.location.host+'/counter',true );
+           request.open('GET','http://sudha87.imad.hasura-app.io/counter',true );
+           request.send(null);
+            
+        }
     
 
 
